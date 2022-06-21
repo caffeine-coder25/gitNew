ed0b5072475dca9e12e1b06c2f0c64d46f51e234
Shubham Saxena <shubham.saxena01@indiamart.com>
2022-06-21 13:50:18 +0530

    “test”

--- file2.cpp
+++ file2.cpp
@@ -1,16 +1,1 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-
-
-int main() {
-	
-#ifndef ONLINE_JUDGE
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-#endif 
-	
-
-	
-	return 0;
-}
+new changes
