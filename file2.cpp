f16a86142c7ac259c5b3d278e053a4a50305f7f9
Shubham Saxena <shubham.saxena01@indiamart.com>
2022-06-21 13:51:21 +0530

    “update”

--- file2.cpp
+++ file2.cpp
@@ -1,1 +1,27 @@
-new changes
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define mod 1e9 + 7
+#define inf 1e18
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define w(x) int x; cin >> x; while(x--)
+
+void caffeine_coder25() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	//#ifndef ONLINE_JUDGE
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	//#endif 
+}
+
+void solve(){
+	
+}
+
+int32_t main() {
+	caffeine_coder25();
+
+	solve();
+	return 0;
+}
